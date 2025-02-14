6017d36220db3aeebc45431d081db286d842ce64
Yura Push <youdeimos1@gmail.com>
2025-02-14 19:11:49 +0200

    add permission classer for CreateUserView

--- user/views.py
+++ user/views.py
@@ -9,6 +9,7 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = ()
 
 
 class CreateTokenView(ObtainAuthToken):
