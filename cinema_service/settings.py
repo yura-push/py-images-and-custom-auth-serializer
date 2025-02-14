01f825e5f575340dbac9acee72191f2b67261f0b
Yura Push <youdeimos1@gmail.com>
2025-02-14 19:11:02 +0200

    set permissions for project

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -141,3 +141,12 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    )
+}
