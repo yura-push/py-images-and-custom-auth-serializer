b98a8c56a05aebb48162498aaacaf9e893ee6281
Yura Push <youdeimos1@gmail.com>
2025-02-17 16:44:01 +0200

    change keyword argument in authenticate metod in AuthTokenSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -55,7 +55,7 @@ class AuthTokenSerializer(serializers.Serializer):
 
         if email and password:
             user = authenticate(request=self.context.get("request"),
-                                email=email, password=password)
+                                username=email, password=password)
 
             if not user:
                 msg = _("Unable to log in with provided credentials.")
